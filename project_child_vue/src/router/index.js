3f2e7e81f4ad1d6297560d2d0431e610796633d2
michael <oy.xt@qq.com>
2022-07-21 10:25:56 +0800

    feat：微前端子应用代码改造

--- project_child_vue/src/router/index.js
+++ project_child_vue/src/router/index.js
@@ -6,16 +6,13 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "/vue",
     name: "home",
     component: HomeView,
   },
   {
-    path: "/about",
+    path: "/vue/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
@@ -23,7 +20,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  // base: "/vue",
   routes,
 });
 
--- project_child_vue/vue.config.js
+++ project_child_vue/vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  lintOnSave: false, // 关闭eslint检测
   transpileDependencies: true,
+  devServer: {
+    port: 8081,
+    headers: {
+      // 因为qiankun内部请求都是fetch来请求资源，所以子应用必须允许跨域
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  configureWebpack: {
+    output: {
+      // 资源打包路径
+      library: 'vueApp',
+      libraryTarget: 'umd'
+    }
+  }
 });
